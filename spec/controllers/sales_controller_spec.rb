d2ca456e01ab52cdafecbc266d493fb9a861b974
Batraisk <AndreyK-ov@yandex.ru>
2017-04-19 13:47:12 +0300

    add test

--- spec/controllers/sales_controller_spec.rb
+++ spec/controllers/sales_controller_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe SalesController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    it "should be 422" do
       get :index
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "returns http success" do
+      get :index, params: {to: '11.04.2014', from: '11.11.2017'}
       expect(response).to have_http_status(:success)
     end
   end
