050eb90e8f8ca200dafc7159b6a82e843a702cc4
Batraisk <AndreyK-ov@yandex.ru>
2017-04-19 13:22:16 +0300

    add controller

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,4 +1,28 @@
 class SalesController < ApplicationController
+	before_action :require_error 
+
   def index
+    to = date_params[:to].to_date
+    from = date_params[:from].to_date
+  	sales = Sale.where(date: from..to).group(:good).sum(:total)
+    goods = []
+    sales.each {|good, revenue| goods.push({ title: good.title, revenue: revenue})}
+  	render json: {from: from, to: to, goods: goods}
   end
+
+	def date_params
+		params.permit(:to,:from)
+	end
+
+	def require_error
+		if date_params[:from].to_date > date_params[:to].to_date
+      render status: 422
+    end
+	end
 end
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'sales/index'
+  get 'sales/' =>'sales#index'
+  get 'sales/index' =>'sales#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
